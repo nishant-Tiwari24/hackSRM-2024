d76996230a5559ce65fc90b63d37842620641602
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-11-10 22:47:59 +0530

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -11,14 +11,14 @@ const HeroContent = () => {
         variants={slideInFromTop}
         className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'>
         <AiOutlineCoffee className='text-[#b49bff] mr-[10px] h-5 w-10'/>
-        <h1 className='Welcome-text text-[13px] mr-[10px] sourcecode'>A 24 Hour Hackathon</h1>
+        <h1 className='Welcome-text text-[14px] mr-[10px] sourcecode'>IDEATE. IMPLEMENT. INNOVATE</h1>
         </motion.div>
 
         <motion.div
         variants={slideInFromLeft(0.5)}
         className='flex flex-col gap-3 mt-6 text-6xl text-bold-900 text-white max-w-[600px] w-auto h-auto sourcecodebold'
         >Why you need to
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-#7944E6-500 to-green-500 bg-gradient-to-r from-fuchsia-500 to-cyan-500'>participate in</span>
+        <span className='text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-cyan-500'>participate in</span>
         in HackSRM?
         </motion.div>
 
@@ -40,6 +40,7 @@ const HeroContent = () => {
         className='button-primary py-[8px] px-[4px] border border-[#002df58b] w-60'>
         {/* <AiOutlineCoffee className='text-white mr-[10px] h- w-10'/> */}
         <h1 className='Welcome-text text-[15px] mr-[10px] sourcecode'>Join Discord</h1>
+
         </motion.button>
         {/* </motion.div> */}
       </div>
