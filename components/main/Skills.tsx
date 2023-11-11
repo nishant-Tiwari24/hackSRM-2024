2713b076f84cd3b5b0de24479f917761724b0ff1
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-11-11 11:05:31 +0530

    skills

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,9 +1,18 @@
+import { transform } from 'next/dist/build/swc'
 import React from 'react'
 
 const Skills = () => {
   return (
     <div>
-      
+      <section
+      id='skills'
+      className='flex flex-col gap-3 h-full relative overflow-hidden py-20'
+      style={{transform:'scale(0.9)'}}
+      >
+        <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
+
+        </div>
+      </section>
     </div>
   )
 }
---
+++ components/sub/SkillDataProvider.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+const SkillDataProvider = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SkillDataProvider
