c465370fd319a0456476a10aec84f6d25a301dc0
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-11-12 10:13:53 +0530

    footer modificatios

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full h-[65px] fixed top-0 items-center flex flex-row m-auto px-[10px] self-center left-[450px] z-[1]'>
+    <div className='w-full h-[65px] fixed top-0 items-center flex flex-row m-auto px-[10px] self-center left-[450px] z-[40]'>
     <div className='w-[500px] h-[55px] flex flex-row items-center justify-between md:mr-20'>
         <div className='flex flex-row items-center justify-between border border-[#7042f861] mr-[15px] px-[20px] py-[10px] rounded-full text-green-200 gap-[100px]'>
             <a href="#about-me" className='cursour-pointer sourcecode'>About</a>
